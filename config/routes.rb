98f009dc1b6f4584e2ae657b555dd3e00340e5b2
Serena Leung <serenaleung1@gmail.com>
2017-03-29 13:44:09 -0700

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,18 @@ Rails.application.routes.draw do
    get('/contact', {to: 'contact#index'})
    post('/contact', {to: 'contact#create', as: 'contact_submit'})
 
+#  (HTTP Verb: delete - path: /questions/:id ) ==> questions controller / destroy action
+#  (HTTP Verb: get - path: /questions/:id/edit ) ==> questions controller / edit action
+#  (HTTP Verb: get - path: /questions/:id ) ==> questions controller / show action
+#  (HTTP Verb: post - path: /questions/:id/comments ) ==> comments controller / create action
+#  (HTTP Verb: get - path: /faq ) ==> home controller / faq action
+
+   delete('/questions/:id', {to: 'questions#destroy'})
+   get('/questions/:id/edit', {to: 'questions#edit'})
+   get('/questions/:id', {to: 'questions#show'})
+   post('/questions/:id/comments', {to: 'comments#create'})
+   get('/faq',{to: 'home#faq'})
+
+
   root 'welcome#index'
 end
